76062a1b7f7ebe5e50277c1b1b07cd2df0abd196
Nithya <100074248+123nithya@users.noreply.github.com>
2024-03-05 16:03:00 +0530

    Delete Day11 directory

--- Day11/src/main/java/com/chessacademy/nithya/service/CourseService.java
+++
@@ -1,7 +0,0 @@
-package com.chessacademy.nithya.service;
-
-import com.chessacademy.nithya.model.Courses;
-
-public interface CourseService {
-    Courses addCourse(Courses course, String institutionId);
-}
--- Day11/src/main/java/com/chessacademy/nithya/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.chessacademy.nithya.service;
-
-import java.security.Principal;
-import com.chessacademy.nithya.dto.request.PasswordRequest;
-
-public interface UserService {
-
-    void forgotPassword(PasswordRequest request, Principal principal);
-}
